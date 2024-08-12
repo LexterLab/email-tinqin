359d674119c1be2d1702490fcfa06d76fa8660e5
moldovexc <alexanderparpulansky@gmail.com>
2024-08-12 17:50:57 +0300

    Export Send Recovery email to feign

--- api/src/main/java/com/tinqinacademy/emails/api/RestExportRouts.java
+++ api/src/main/java/com/tinqinacademy/emails/api/RestExportRouts.java
@@ -2,4 +2,5 @@ package com.tinqinacademy.emails.api;
 
 public class RestExportRouts {
 	public static final String CONFIRM_EMAIL = "POST " + RestAPIRoutes.CONFIRMATION_EMAIL;
+	public static final String RECOVERY_EMAIL = "POST " + RestAPIRoutes.RECOVERY_EMAIL;
 }
